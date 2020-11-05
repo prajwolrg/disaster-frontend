8aa45b06600361990e922c5be5ec801df9c64752
Prajwol Gyawali <prajwol.lucifer@gmail.com>
2020-11-05 14:04:21 +0545

    Initial Commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+    GridComponent,
+    ColumnDirective,
+    ColumnsDirective,
+    Page,
+    Inject,
+    Edit,
+    Toolbar,
+    Filter,
+} from "@syncfusion/ej2-react-grids";
+import data from "./disaster_data.json";
+
+import DialogForm from "./dialogForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const editOptions = {
+        allowEditing: true,
+        allowAdding: true,
+        allowDeleting: true,
+        // mode: "Dialog",
+        // template: dialogTemplate,
+    };
+    const toolbarOptions = ["Add", "Edit", "Delete", "Update", "Cancel"];
+
+    const filterOptions = {
+        ignoreAccent: true,
+        type: "Menu",
+    };
+
+    function dialogTemplate(props) {
+        return <DialogForm {...props} />;
+    }
+    return (
+        <div style={{ margin: "10%", marginTop: "5%" }}>
+            <GridComponent
+                dataSource={data}
+                allowPaging={true}
+                pageSettings={{ pageSize: 20 }}
+                editSettings={editOptions}
+                toolbar={toolbarOptions}
+                allowFiltering={true}
+                filterSettings={filterOptions}
+            >
+                <ColumnsDirective>
+                    <ColumnDirective
+                        field="IncidentDate"
+                        type="Date"
+                        editType="datepickeredit"
+                        format="yMd"
+                        edit={false}
+                    />
+                    <ColumnDirective field="District" editType="dropdownedit" />
+                    <ColumnDirective field="Incident" editType="dropdownedit" />
+                    <ColumnDirective field="TotalDeath" />
+                    <ColumnDirective field="MissingPeople" />
+                    <ColumnDirective field="AffectedFamily" />
+                    <ColumnDirective field="EstimatedLoss" />
+                    <ColumnDirective field="Injured" />
+                    <ColumnDirective field="HousesDamaged" />
+                </ColumnsDirective>
+                <Inject services={[Page, Edit, Toolbar, Filter]} />
+            </GridComponent>
+        </div>
+    );
 }
 
 export default App;
