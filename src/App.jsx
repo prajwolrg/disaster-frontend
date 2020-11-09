9c3dec4d63ca0f3098bfbfc5587d8b7526341b05
Madhav Aryal <074bct520.madhav@pcampus.edu.np>
2020-11-09 19:06:15 +0545

    khai

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { CircularProgress } from "@material-ui/core";
 import React, { Suspense, useContext, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Menus from "./components/Menus";
