5dfc260e6d75b4a9454b9daec91dc8a20b65cdd3
Prajwol Gyawali <prajwol.lucifer@gmail.com>
2020-11-09 14:09:37 +0545

    Date issue fix.

--- src/components/forms/EditIncident.jsx
+++ src/components/forms/EditIncident.jsx
@@ -2,29 +2,40 @@ import React, { useContext } from "react";
 import { Context as ApiContext } from "../../context/ApiContext";
 import IncidentForm from "./IncidentForm";
 
+function formatDate(date) {
+    var d = new Date(date),
+        month = "" + (d.getMonth() + 1),
+        day = "" + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    return [year, month, day].join("-");
+}
 const EditIncident = ({ open, onClose, initialValues }) => {
-  const { updateIncident } = useContext(ApiContext);
+    const { updateIncident } = useContext(ApiContext);
 
-  const { incidentID, disasterTypeName, vmID, incidentDate } = initialValues;
+    const { incidentID, disasterTypeName, vmID, incidentDate } = initialValues;
 
-  const handleEdit = async (values) => {
-    await updateIncident(incidentID, values);
-  };
+    const handleEdit = async (values) => {
+        await updateIncident(incidentID, values);
+    };
 
-  return (
-    <IncidentForm
-      type="Edit"
-      open={open}
-      onClose={onClose}
-      disasterTypeName={disasterTypeName || ""}
-      initialValues={{
-        ...initialValues,
-        locationID: vmID,
-        incidentDate: new Date(incidentDate),
-      }}
-      submitFunction={handleEdit}
-    />
-  );
+    return (
+        <IncidentForm
+            type="Edit"
+            open={open}
+            onClose={onClose}
+            disasterTypeName={disasterTypeName || ""}
+            initialValues={{
+                ...initialValues,
+                locationID: vmID,
+                incidentDate: formatDate(initialValues.incidentDate),
+            }}
+            submitFunction={handleEdit}
+        />
+    );
 };
 
 export default EditIncident;
